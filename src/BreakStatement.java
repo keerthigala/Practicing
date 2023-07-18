c616f2ebe07511c2eff3b94c2f9cc52030f51e95
keerthigala <galakeerthi@gmail.com>
2023-07-18 22:46:53 +0530

    third commit

---
+++ src/BreakStatement.java
@@ -0,0 +1,22 @@
+public class BreakStatement {
+    public static void main(String[] args) {
+        switch ("Thr") {
+            case "Mon":
+                System.out.print("Monday");
+                break;
+            case "Tue":
+                System.out.print("Tuesday");
+                break;
+            case "Wed":
+                System.out.print("Wednesday");
+                break;
+            case "Thr":
+                System.out.print("Thrusday");
+                break;
+            default:
+                System.out.print("No day");
+        }
+
+    }
+}
+
